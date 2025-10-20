d4e18c52aca19fcbaf96fa6f90fd1cfa85d1f5e2
Sonnil Le <sonnil.le@sanofi.com>
2025-10-20 17:59:03 -0400

    fix: move SPA routing handler to JavaScript bundle

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Single Page Apps for GitHub Pages
+// https://github.com/rafgraph/spa-github-pages
+// Handle redirects from 404.html
+(function() {
+  const l = window.location;
+  if (l.search[1] === '/') {
+    const decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, '', l.pathname.slice(0, -1) + decoded + l.hash);
+  }
+})();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
